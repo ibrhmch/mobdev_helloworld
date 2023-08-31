4bbdc44f74dac71def6a1541646175dec8f2fbc5
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-08-31 13:08:25 -0600

    finished homework

--- Hello World/ContentView.swift
+++ Hello World/ContentView.swift
@@ -10,14 +10,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            AsyncImage(url: URL(string: "https://i.ibb.co/5jPgJ7y/IMG-6527.jpg")) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 10)
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Ibrahim!")
+            Text("Hello, This is Ibrahim!")
+            
+            ZStack{
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: 500, height: 50)
+                
+                HStack{
+                    Link("Tap here to email me", destination: URL(string: "mailto:ch.ibrahim@colorado.edu")!)
+                        .foregroundColor(.white)
+                    
+                    Image(systemName: "plus.message.fill")
+                        .background(.blue)
+                        .foregroundColor(.white)
+                    
+                }
+                
+            }
+            
         }
         .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
